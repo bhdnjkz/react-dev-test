61f8a8701e70258ee0be6c733ad7476ea85d5fba
Богдан <norestwithwest@Overlord.local>
2022-02-14 18:50:44 +0200

    added some dependecies for future work and also a button for app.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,15 @@ export default class App extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, friend! This is a front page component for a future application! r
+          Hello, friend! This is a front page component for a future application!
         </p>
+        <button 
+          className="button"
+          type="button"
+          onClick={() => {console.log('clicked')}}
+          >
+            Click me!
+          </button>
       </header>
     </div>
   );
